dfb3a9aab5fa7c8c382e7d897aaa70cafb162a5b
steckes <stephan@neodsp.com>
2025-08-25 21:25:59 +0200

    update user in start script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-/home/pi/llama.cpp/build/bin/llama-server -m /home/pi/knight-rider/gemma-3-270m-it-Q8_0.gguf -c 0 -fa --offline &
-cd /home/pi/knight-rider && cargo run --release &
+TARGET_USER="${SUDO_USER:-$USER}"
+/home/$TARGET_USER/llama.cpp/build/bin/llama-server -m /home/$TARGET_USER/knight-rider/gemma-3-270m-it-Q8_0.gguf -c 0 -fa --offline &
+cd /home/$TARGET_USER/knight-rider && cargo run --release &
