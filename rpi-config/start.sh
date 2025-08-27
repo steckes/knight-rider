3f2546cf010de3f2465cebae3d6af2ae9882823b
steckes <stephan@neodsp.com>
2025-08-27 13:57:56 +0200

    Update start.sh

--- rpi-config/start.sh
+++ rpi-config/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 TARGET_USER="${SUDO_USER:-$USER}"
-/home/$TARGET_USER/llama.cpp/build/bin/llama-server -m /home/$TARGET_USER/knight-rider/gemma-3-270m-it-Q8_0.gguf -c 0 -fa --offline &
+/home/$TARGET_USER/llama.cpp/build/bin/llama-server -m /home/$TARGET_USER/knight-rider/gemma-3-270m-it-Q8_0.gguf -c 0 -fa --offline & # fast model
+#/home/$TARGET_USER/llama.cpp/build/bin/llama-server -m /home/$TARGET_USER/knight-rider/gemma-3-1b-it-Q4_K_M.gguf -c 0 -fa --offline & # slow model
 cd /home/$TARGET_USER/knight-rider && cargo run --release &
